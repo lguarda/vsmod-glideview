5a693fe175a8e16e5f26ff45c3a9e3a888b97f51
lguarda <leoman037@live.fr>
2026-05-06 23:37:01 +0200

    feat: player can still cycle to camera
    but for now mod will still switch camera when changing state
    Also this commit remove debug comment + format with custom clang-format
    config

--- GlideView/GlideViewModSystem.cs
+++ GlideView/GlideViewModSystem.cs
@@ -12,67 +12,49 @@ namespace GlideView {
 public class GlideViewModSystem : ModSystem {
 
     ICoreClientAPI _capi;
+    EnumCameraMode last_mode;
 
-  public bool shouldDoThirdPerson() {
-    var entity =
-        _capi.World.Player
-            .Entity; // this is already EntityPlayer which extends EntityAgent
-
-    //_capi.ShowChatMessage($"1 oooooooooooooooooooo");
-    if (entity.MountedOn != null) {
-      //_capi.ShowChatMessage($"2 oooooooooooooooooooo");
-
-      // var seat = entity.MountedOn;
-      //_capi.ShowChatMessage("MountedOn type: " + seat?.GetType()?.FullName);
-      //_capi.ShowChatMessage("MountSupplier type: " +
-      //seat?.MountSupplier?.GetType()?.FullName);
-      return true;
-
-      //    var mount = entity.MountedOn?.MountSupplier as Entity;
-      //    var path = mount?.Code?.Path;
-      //_capi.ShowChatMessage($"3 oooooooooooooooooooo path{path}
-      //code{mount?.Code} mount{mount} ");
-      //    if (path != null && (path.Contains("elk") || path.Contains("boat")))
-      //    {
-      //_capi.ShowChatMessage($"4 oooooooooooooooooooo");
-      //        return true;
-      //    }
-    } else if (entity.Controls.Gliding) {
-      return true;
+    public bool shouldDoThirdPerson() {
+        var entity = _capi.World.Player.Entity;
+        if (entity.MountedOn != null) {
+            return true;
+        } else if (entity.Controls.Gliding) {
+            return true;
+        }
+        return false;
     }
-    return false;
-  }
 
-  // Tested on 1.22.2
-  // This is not part of public api so i use reflection to fetch the camera then
-  // set the mode
-  public void setCameraMode(ICoreClientAPI capi, EnumCameraMode mode) {
-    var gameField = capi.GetType().GetField("game", BindingFlags.NonPublic |
-                                                        BindingFlags.Instance);
-    var game = gameField.GetValue(capi);
+    // Tested on 1.22.2
+    // This is not part of public api so i use reflection to fetch the camera then
+    // set the mode
+    public void setCameraMode(ICoreClientAPI capi, EnumCameraMode mode) {
+        if (last_mode == mode) {
+            return;
+        }
+        var gameField = capi.GetType().GetField("game", BindingFlags.NonPublic | BindingFlags.Instance);
+        var game = gameField.GetValue(capi);
 
-    var cameraField = game.GetType().GetField(
-        "MainCamera", BindingFlags.Public | BindingFlags.Instance);
-    var camera = cameraField.GetValue(game);
+        var cameraField = game.GetType().GetField("MainCamera", BindingFlags.Public | BindingFlags.Instance);
+        var camera = cameraField.GetValue(game);
 
-    // Call SetMode directly
-    var setMode = camera.GetType().GetMethod(
-        "SetMode", BindingFlags.NonPublic | BindingFlags.Instance);
+        // Call SetMode directly
+        var setMode = camera.GetType().GetMethod("SetMode", BindingFlags.NonPublic | BindingFlags.Instance);
 
-    setMode.Invoke(camera, new object[] { mode });
-  }
+        setMode.Invoke(camera, new object[] { mode });
+        last_mode = mode;
+    }
 
-  public override void StartClientSide(ICoreClientAPI api) {
-    Mod.Logger.Notification("GlideView starting");
-    _capi = api;
+    public override void StartClientSide(ICoreClientAPI api) {
+        Mod.Logger.Notification("GlideView starting");
+        _capi = api;
 
-    _capi.Event.RegisterGameTickListener(dt => {
-      if (shouldDoThirdPerson()) {
-        setCameraMode(_capi, EnumCameraMode.ThirdPerson);
-      } else {
-        setCameraMode(_capi, EnumCameraMode.FirstPerson);
-      }
-    }, 100);
-  }
+        _capi.Event.RegisterGameTickListener(dt => {
+            if (shouldDoThirdPerson()) {
+                setCameraMode(_capi, EnumCameraMode.ThirdPerson);
+            } else {
+                setCameraMode(_capi, EnumCameraMode.FirstPerson);
+            }
+        }, 100);
+    }
 }
 }
